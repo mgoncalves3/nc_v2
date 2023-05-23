24516d2e7b41de389aa65a909e84f8958e848ee2
mgoncalves3 <marco.g@sapo.pt>
2023-05-23 23:10:18 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+require("dotenv").config();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`)
+});
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  console.log(req.method)
+  
+})
